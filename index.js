91b502aa7f7d9940ba2978de2b27c69a408003f8
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-07-06 21:40:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+/* BrayanRojasTV */
+/*               */
+/*   SudoReal    */
+/*               */
+/* Main code for */
+/* the website,  */
+/* don't delete! */
+
+/* Javascript principal para su página. */
+
+
+
+/* i'll do something here soon, don't delete this! */
